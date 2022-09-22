d370a474d894bc449e00652e2bcf5917dd5ec8a4
madeleinebien <madeleinebien.angeles.cics@ust.edu.ph>
2022-09-22 22:06:14 +0800

    FIFTH COMMIT

--- newproject_3.py
+++ newproject_3.py
@@ -5,16 +5,22 @@ def get_ip():
     response = requests.get('https://api64.ipify.org?format=json').json()
     return response["ip"]
 
-
+#Get the ip, ip version, city, region, country, and country code of the user's current IP Address using IPAPI API 
 def get_location():
     ip_address = get_ip()
     response = requests.get(f'https://ipapi.co/{ip_address}/json/').json()
     location_data = { 
+        #Get the IP Address
         "ip": ip_address,
+        #Get the Version of the IP  Address
         "version": response.get("version"),
+        #Get the city of the IP Address
         "city": response.get("city"),
+        #Get the region of the IP Address
         "region": response.get("region"),
+        #Get the country of the IP Address
         "country": response.get("country_name"),
+        #Get the country code of the IP Address
         "country_code": response.get("country_code")
         
         
@@ -23,12 +29,3 @@ def get_location():
 
 
 print(get_location())
-
-def get_current_ipv6():
-    try:
-        return requests.get("https://api6.ipify.org", timeout=5).text
-    except requests.exceptions.ConnectionError as ex:
-        return None
-
-print(get_current_ipv6())
-  
