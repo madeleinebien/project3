e0d2f1492914b091428d610e2fe36c12f48cb6d8
madeleinebien <madeleinebien.angeles.cics@ust.edu.ph>
2022-09-23 15:44:25 +0800

    7th Commit

--- newproject_3.py
+++ newproject_3.py
@@ -11,29 +11,29 @@ while True:
     dest = input("Destination: ")
     if dest == "quit" or dest == "q":
         break
-
+    #Get requests from the API 
     url = main_api + urllib.parse.urlencode({"key":key, "from":orig, "to":dest})
-
     json_data = requests.get(url).json()
     json_data = requests.get(url).json()
+    #Retrieves the status code of the location and destination from the MapQuest API
     json_status = json_data["info"]["statuscode"]
-
+    #If else based on the status code of the inputted location and destination 
+    #0 indicates a successful route call
     if json_status == 0:
         print("______________________________________________")
-        
-    
         print("API Status: " + str(json_status) + " = A successful route call.")
         print("______________________________________________")
         print()
+    #Displays the entered original location and destination in uppercase 
         message = "Directions from " + (orig)  + " to " + (dest)
         print(message.upper())
-       
+    #Displays the distance of the location and destination 
         print("Distance: " + str("{:.2f}".format((json_data["route"]["distance"])*3.78)))
-
+    #Displays the duration of the trip in time format 
         print("Trip Duration:   " + (json_data["route"]["formattedTime"]))
-
+    #Displays the kilometers 
         print("Kilometers:      " + str("{:.2f}".format((json_data["route"]["distance"])*1.61)))
-
+    #Displays the fuel used in Liters
         print("Fuel Used (Ltr): " + str("{:.2f}".format((json_data["route"]["fuelUsed"])*3.78)))
         
    
@@ -45,17 +45,31 @@ while True:
 
             print((each["narrative"]) + " (" + str("{:.2f}".format((each["distance"])*1.61) + " km)"))
         print("=============================================\n")
-
+    #402 indicates an invalid location
     elif json_status == 402:
-
         print("**********************************************")
         print("Status Code: " + str(json_status) + "; Invalid user inputs for one or both locations.")
         print("**********************************************\n")
+    #611 indicates that only one location has been entered by the user 
     elif json_status == 611:
         print("**********************************************")
         print("Status Code: " + str(json_status) + "; Missing an entry for one or both locations.")
         print("**********************************************\n")
-
+    #500 indicates that there is an unknown error  
+    elif json_status == 500:
+        print("**********************************************")
+        print("Status Code: " + str(json_status) + "; Unknown Error.")
+        print("**********************************************\n")
+    #606 indicates that the user has exceeded the maximum number of locations 
+    elif json_status == 606:
+        print("**********************************************")
+        print("Status Code: " + str(json_status) + "; Exceeded the maximum search distance.")
+        print("**********************************************\n")
+    #612 indicates that there are no routes available for the entered locations 
+    elif json_status == 612:
+        print("**********************************************")
+        print("Status Code: " + str(json_status) + "; There are no routes available for these locations.")
+        print("**********************************************\n")
     else:
 
         print("************************************************************************")
